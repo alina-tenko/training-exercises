30505f041288d568b94e9357e46281f24732fe71
Alina Kondratenko <alinakondratenko92@gmail.com>
2021-07-14 17:38:56 +0300

    added new exception examples

--- golovach-exceptions/src/main/java/ua/training/kondratenko/ExceptionExamples.java
+++ golovach-exceptions/src/main/java/ua/training/kondratenko/ExceptionExamples.java
@@ -366,3 +366,222 @@ class App22 {
 }
 //>> Это RuntimeException на самом деле!!!
 
+class App23 {
+    public static void main(String[] args) throws Exception { // пока игнорируйте 'throws'
+        try {
+            System.err.print(" 0");
+            if (true) {throw new Exception();}
+            System.err.print(" 1");
+        } catch (RuntimeException e) {
+            System.err.print(" 2");
+        }
+        System.err.print(" 3");
+    }
+}
+//>> 0
+//>> RUNTIME EXCEPTION: Exception in thread "main" java.lang.Exception
+
+class App24 {
+    public static void main(String[] args) {
+        try {
+            System.err.print(" 0");
+            if (true) {throw new Error();}
+            System.err.print(" 1");
+        } catch (Exception e) {
+            System.err.print(" 2");
+        }
+        System.err.print(" 3");
+    }
+}
+//>> 0
+//>> RUNTIME EXCEPTION: Exception in thread "main" java.lang.Error
+
+class App25 {
+    public static void main(String[] args) {
+        try {
+            System.err.print(" 0");
+            if (true) {throw new RuntimeException();}
+            System.err.print(" 1");
+        } catch (RuntimeException e) {     // перехватили RuntimeException
+            System.err.print(" 2");
+            if (true) {throw new Error();} // но бросили Error
+        }
+        System.err.println(" 3");          // пропускаем - уже летит Error
+    }
+}
+//>> 0 2
+//>> RUNTIME EXCEPTION: Exception in thread "main" java.lang.Error
+
+class App26 {
+    public static void main(String[] args) {
+        try {
+            System.err.print(" 0");
+            if (true) {throw new RuntimeException();}
+            System.err.print(" 1");
+        } catch (RuntimeException e) { // перехватили RuntimeException
+            System.err.print(" 2.1");
+            try {
+                System.err.print(" 2.2");
+                if (true) {throw new Error();} // и бросили новый Error
+                System.err.print(" 2.3");
+            } catch (Throwable t) {            // перехватили Error
+                System.err.print(" 2.4");
+            }
+            System.err.print(" 2.5");
+        } catch (Error e) { // хотя есть cath по Error "ниже", но мы в него не попадаем
+            System.err.print(" 3");
+        }
+        System.err.println(" 4");
+    }
+}
+//>> 0 2.1 2.2 2.4 2.5 4
+
+class App27 {
+    public static void main(String[] args) {
+        try {
+            throw new Exception();
+        } catch (RuntimeException e) {
+            System.err.println("catch RuntimeException");
+        } catch (Exception e) {
+            System.err.println("catch Exception");
+        } catch (Throwable e) {
+            System.err.println("catch Throwable");
+        }
+        System.err.println("next statement");
+    }
+}
+//>> catch Exception
+//>> next statement
+
+class App28 {
+    public static void main(String[] args) {
+        try {
+            Throwable t = new Exception(); // ссылка типа Throwable указывает на объект типа Exception
+            throw t;
+        } catch (RuntimeException e) {
+            System.err.println("catch RuntimeException");
+        } catch (Exception e) {
+            System.err.println("catch Exception");
+        } catch (Throwable e) {
+            System.err.println("catch Throwable");
+        }
+        System.err.println("next statement");
+    }
+}
+//>> catch Exception
+//>> next statement
+
+class App29 {
+    public static void main(String[] args) {
+        try {
+            System.err.println("try");
+        } finally {
+            System.err.println("finally");
+        }
+    }
+}
+//>> try
+//>> finally
+
+class App30 {
+    public static void main(String[] args) {
+        try {
+            System.err.println("try");
+            if (true) {throw new RuntimeException();}
+        } finally {
+            System.err.println("finally");
+        }
+        System.err.println("more");
+    }
+}
+//>> try
+//>> finally
+//>> Exception in thread "main" java.lang.RuntimeException
+
+class App31 {
+    public static void main(String[] args) {
+        System.err.println(f());
+    }
+    public static int f() {
+        try {
+            return 0;
+        } finally {
+            return 1;
+        }
+    }
+}
+//>> 1
+
+//class App32 {
+//    public static void main(String[] args) {
+//        System.err.println(f());
+//    }
+//    public static int f() {
+//        long rnd = System.currenttimeMillis();
+//        boolean finished = false;
+//        try {
+//            if (rnd % 3 == 0) {
+//                throw new Error();
+//            } else if (rnd % 3 == 1) {
+//                throw new RuntimeException();
+//            } else {
+//                // nothing
+//            }
+//            finished = true;
+//        } finally {
+//            if (finished) {
+//                // не было исключений
+//            } else {
+//                // было исключение, но какое?
+//            }
+//        }
+//    }
+//}
+
+class App33 {
+    public static void main(String[] args) {
+        try {
+            System.err.print(" 0");
+            try {
+                System.err.print(" 1");
+                // НИЧЕГО
+                System.err.print(" 2");
+            } catch (RuntimeException e) {
+                System.err.print(" 3"); // НЕ заходим - нет исключения
+            } finally {
+                System.err.print(" 4"); // заходим всегда
+            }
+            System.err.print(" 5");     // заходим - выполнение в норме
+        } catch (Exception e) {
+            System.err.print(" 6");     // НЕ заходим - нет исключения
+        } finally {
+            System.err.print(" 7");     // заходим всегда
+        }
+        System.err.print(" 8");         // заходим - выполнение в норме
+    }
+}
+//>> 0 1 2 4 5 7 8
+
+class App34 {
+    public static void main(String[] args) {
+        try {
+            System.err.print(" 0");
+            try {
+                System.err.print(" 1");
+                if (true) {throw new Exception();}
+                System.err.print(" 2");
+            } catch (RuntimeException e) {
+                System.err.print(" 3"); // НЕ заходим - есть исключение, но НЕПОДХОДЯЩЕГО ТИПА
+            } finally {
+                System.err.print(" 4"); // заходим всегда
+            }
+            System.err.print(" 5");     // не заходим - выполнение НЕ в норме
+        } catch (Exception e) {
+            System.err.print(" 6");     // ЗАХОДИМ - есть подходящее исключение
+        } finally {
+            System.err.print(" 7");     // заходим всегда
+        }
+        System.err.print(" 8");         // заходим - выполнение УЖЕ в норме
+    }
+}
+//>> 0 1 4 6 7 8
